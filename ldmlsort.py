5b3a8580dfc01f0e0e0a4ed4c924b8a4f5847f18
Wes <wes_peacock@sil.org>
2021-11-04 14:12:44 -0600

    Add nonorm flag & remove default ldml file
     * sets default to [normalization on]
     * makes ldml file optional
     * will use DUCET if no ldml file

--- ldmlsort.py
+++ ldmlsort.py
@@ -2,15 +2,19 @@
 import argparse
 import sys, icu
 parser = argparse.ArgumentParser()
-parser.add_argument("--ldml", help="ldml file name", default="default.ldml")
+parser.add_argument("--ldml", help="ldml file name")
 parser.add_argument("--debug", help="debug mode", action='store_true')
+parser.add_argument("--nonorm", help="do not normalize input unless specified in ldml", action='store_true')
 args = parser.parse_args()
 if args.debug and args.ldml: print(args.ldml, file=sys.stderr)
+stringnorm = '' if args.nonorm else '[normalization on]\n'
 from lxml import etree
-ldmltree = etree.parse(args.ldml)
-collxpath = "/ldml/collations/collation[@type=\"standard\"]/cr/text()"
-coll = ldmltree.xpath(collxpath)
-stringcoll = "".join(coll)
+coll = ""
+if args.ldml:
+    ldmltree = etree.parse(args.ldml)
+    collxpath = "/ldml/collations/collation[@type=\"standard\"]/cr/text()"
+    coll = ldmltree.xpath(collxpath)
+stringcoll = stringnorm+"".join(coll)
 if args.debug: print(stringcoll, file=sys.stderr)
 lines = sys.stdin.readlines()
 rbc = icu.RuleBasedCollator(stringcoll)
