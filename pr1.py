143d5cc92ec3c0565371b10a26dd119edd80c38f
Wes <wes_peacock@sil.org>
2021-12-09 10:33:15 -0700

    Remove extraneous python files

--- pr1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import sys, icu
-lines = sys.stdin.readlines()
-rbc = icu.RuleBasedCollator('&a < d\n&b < f')
-lines = sorted(lines, key=rbc.getSortKey)
-sys.stdout.write(''.join(lines))
